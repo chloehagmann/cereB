e4434d5dc849c7308232609da66a87106cccf135
Irene <ijpark@usc.edu>
2022-02-19 20:52:52 -0800

    merge

--- src/main.js
+++ src/main.js
@@ -22,21 +22,21 @@ document.querySelector("#downArrow").onclick = function(event) {
     document.getElementById("todo").scrollIntoView(true);
 }
 
-let todoArray = [];
-
 let storedTodo = localStorage.getItem("todo");
 if (storedTodo) {
     let todos = JSON.parse(storedTodo);
-    todoArray = todos;
+    for (let i=0; i < todos.length; i++) {
+        document.querySelector("#list").innerHTML += `<li class="listItem">${todos[i]}</li>`;
+    }
 }
+
+let todoArray = [];
 
 document.querySelector("#todo-form").onsubmit = function(event) {
     event.preventDefault();
 
     console.log("submittodo");
-    
     let todoInput = document.querySelector("#todo-input").value.trim();
-    
     todoArray.push(todoInput);
 
     let todoString = JSON.stringify(todoArray);
@@ -44,13 +44,14 @@ document.querySelector("#todo-form").onsubmit = function(event) {
     localStorage.setItem("todo", todoString);
 
     document.querySelector("#list").innerHTML = "";
+
     for (let i=0; i < todoArray.length; i++) {
-        document.querySelector("#list").innerHTML += `<li>${todoArray[i]}</li>`;
+        document.querySelector("#list").innerHTML += `<li class="listItem">${todoArray[i]}</li>`;
     }
+    
+    document.querySelector("#todo-input").value = "";
 }
 
-let listArray = document.querySelectorAll("li");
-console.log(listArray);
 
 for (let i=0; i < listArray.length; i++) {
     listArray[i].onclick = function () {
@@ -58,90 +59,3 @@ for (let i=0; i < listArray.length; i++) {
         this.remove();
     }
 }
-
-
-var pomodoro = {
-    started : false,
-    minutes : 0,
-    seconds : 0,
-    fillerHeight : 0,
-    fillerIncrement : 0,
-    interval : null,
-    minutesDom : null,
-    secondsDom : null,
-    fillerDom : null,
-    init : function(){
-      var self = this;
-      this.minutesDom = document.querySelector('#minutes');
-      this.secondsDom = document.querySelector('#seconds');
-      this.fillerDom = document.querySelector('#filler');
-      this.interval = setInterval(function(){
-        self.intervalCallback.apply(self);
-      }, 1000);
-      document.querySelector('#work').onclick = function(){
-        self.startWork.apply(self);
-      };
-      document.querySelector('#shortBreak').onclick = function(){
-        self.startShortBreak.apply(self);
-      };
-      document.querySelector('#longBreak').onclick = function(){
-        self.startLongBreak.apply(self);
-      };
-      document.querySelector('#stop').onclick = function(){
-        self.stopTimer.apply(self);
-      };
-    },
-    resetVariables : function(mins, secs, started){
-      this.minutes = mins;
-      this.seconds = secs;
-      this.started = started;
-      this.fillerIncrement = 200/(this.minutes*60);
-      this.fillerHeight = 0;  
-    },
-    startWork: function() {
-      this.resetVariables(25, 0, true);
-    },
-    startShortBreak : function(){
-      this.resetVariables(5, 0, true);
-    },
-    startLongBreak : function(){
-      this.resetVariables(15, 0, true);
-    },
-    stopTimer : function(){
-      this.resetVariables(25, 0, false);
-      this.updateDom();
-    },
-    toDoubleDigit : function(num){
-      if(num < 10) {
-        return "0" + parseInt(num, 10);
-      }
-      return num;
-    },
-    updateDom : function(){
-      this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
-      this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
-      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
-      this.fillerDom.style.height = this.fillerHeight + 'px';
-    },
-    intervalCallback : function(){
-      if(!this.started) return false;
-      if(this.seconds == 0) {
-        if(this.minutes == 0) {
-          this.timerComplete();
-          return;
-        }
-        this.seconds = 59;
-        this.minutes--;
-      } else {
-        this.seconds--;
-      }
-      this.updateDom();
-    },
-    timerComplete : function(){
-      this.started = false;
-      this.fillerHeight = 0;
-    }
-};
-window.onload = function(){
-  pomodoro.init();
-};
